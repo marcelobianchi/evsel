9442aaf8654348b5a5f626bb795f4d8a8fc5bb97
Marcelo Bianchi <m.tchelo@gmail.com>
2014-05-24 17:16:49 -0300

    Added checkinstall, a script that check for the necessary, basic tools on the system before install

---
+++ checkinstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+status=0
+for tool in awk cat wget rm sort wc mv sed mkdir
+do
+	which $tool > /dev/null 2>&1
+	[ $? -ne 0 ] && echo "Failed $tool" && status=1
+done
+exit $status
+
